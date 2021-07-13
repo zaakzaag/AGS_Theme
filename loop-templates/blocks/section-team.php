e9a5891bd4012c6bfd033de2dea50284d57e1d9f
Maciej <mocador23@o2.pl>
2021-07-13 09:18:09 +0200

    Team as ACF Block

--- loop-templates/blocks/section-team.php
+++ loop-templates/blocks/section-team.php
@@ -27,36 +27,32 @@ if(!empty($block['align'])){
 <section class="section  section-team">
     <div class=" wrapper mw-100 px-0">
         <div class="row m-0  pt-0 pb-5">
-        <div class="col-lg-4 col-md-6 person-column d-flex align-items-end">                
-                <h5>Tom Nowak</h5>
 
-                <div class="person-info d-flex d-flex justify-content-center">
-                    <h5>Tom Nowak</h5>
-                    <div class="person-position">Super pracownik</div>
-                    <div class="person-desc">tur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam</div>
+        <?php if( have_rows('team_repeater') ): ?>
+        
+            <?php while( have_rows('team_repeater') ): the_row(); 
+            $name_and_surname = get_sub_field('name_and_surname');
+            $job_position = get_sub_field('job_position');
+            $person_description = get_sub_field('person_description');
+            $background_image = get_sub_field('picture');
+            ?>
 
+
+
+                <div class="col-lg-4 col-md-6 person-column d-flex align-items-end" style="background-image:url(<?php echo $background_image['url']?>)">                
+                    <h5><?php echo $name_and_surname; ?></h5>
+
+                    <div class="person-info d-flex d-flex justify-content-center">
+                        <h5><?php echo $name_and_surname; ?></h5>
+                        <div class="person-position"><?php echo $job_position; ?></div>
+                        <div class="person-desc"><?php echo $person_description; ?></div>
+
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-6 person-column d-flex align-items-end">                
-                <h5>Tom Nowak</h5>
 
-                <div class="person-info d-flex d-flex justify-content-center">
-                    <h5>Tom Nowak</h5>
-                    <div class="person-position">Super pracownik</div>
-                    <div class="person-desc">tur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam</div>
+            <?php endwhile; ?>
+        <?php endif; ?>
 
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 person-column d-flex align-items-end">                
-                <h5>Tom Nowak</h5>
-
-                <div class="person-info d-flex d-flex justify-content-center">
-                    <h5>Tom Nowak</h5>
-                    <div class="person-position">Super pracownik</div>
-                    <div class="person-desc">tur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam</div>
-
-                </div>
-            </div>
         </div>
     </div>
 </section>
